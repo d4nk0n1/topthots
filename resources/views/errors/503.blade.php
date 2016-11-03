6b58089b68d5372d7792efbbfb21f60d5e70fffa
dankoni <d4nk0n1@gmail.com>
2016-11-02 21:54:33 -0400

    update

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Be right back.</title>
-
+        <title><i class="fa fa-twitter"></i>@topthotsdot</title>
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 
         <style>
             html, body {
@@ -16,7 +20,7 @@
                 width: 100%;
                 color: #F8B8C8;
                 display: table;
-                font-weight: 100;
+                font-weight: bolder;
                 font-family: 'Lato', sans-serif;
             }
 
@@ -32,16 +36,16 @@
             }
 
             .title {
-                font-size: 72px;
-                margin-bottom: 40px;
+                font-size: 80px;
+                margin-bottom: 8px;
             }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <
-                <div class="title">Be right back.</div>
+                <div class="title"><i class="fa fa-twitter"></i>@top<span style="color: #59fff9;font-weight: bolder;">thots</span>dot</div>
+                <img style="width:350px;" src="/img/mulaney.jpg" />
             </div>
         </div>
     </body>
