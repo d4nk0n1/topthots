1acd17120f9d4d13f07febc1130ecf09f523459b
Matthew Speicher <mspeicher@whitelabelcomm.com>
2017-02-21 10:15:40 -0500

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,8 @@
     <div class="content">
         <div class="title"><i style="color: #59fff9;" class="fa fa-twitter"></i>@top<span style="color: #59fff9;font-weight: bolder;">thots</span>dot</div>
         <a href="http://twitter.com/topthotsdot" target="_blank">
-            <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/topthotsdot">Tweets by topthotsdot</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr"><a href="https://twitter.com/hashtag/TopThot?src=hash">#TopThot</a> award to <a href="https://twitter.com/SallySquirt">@sallysquirt</a> for best &quot;omg cum on my little girl face&quot; face 💯😻<a href="https://twitter.com/karlokarrera">@karlokarrera</a> <a href="https://twitter.com/mofosnetwork">@mofosnetwork</a> <a href="https://twitter.com/hashtag/dontbreakme?src=hash">#dontbreakme</a> <a href="https://t.co/FvDvDgrRui">pic.twitter.com/FvDvDgrRui</a></p>&mdash; TopThots.com (@topthotsdot) <a href="https://twitter.com/topthotsdot/status/814771886731173888">December 30, 2016</a></blockquote>
+            <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
         </a>
     </div>
 </div>
