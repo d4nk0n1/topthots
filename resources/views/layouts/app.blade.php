8120daa9be41d9f1fe810076c5e2d568c4050ba5
Matthew Speicher <mspeicher@whitelabelcomm.com>
2017-04-04 18:35:24 -0400

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        <img src="/img/topthotssmall.png">
                     </a>
                 </div>
 
